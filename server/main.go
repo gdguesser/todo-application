b6a6507ab1337c309bc80865ba4f8a55fc9070f8
Gabriel Guesser <gdguesser@gmail.com>
2022-09-17 13:34:37 +0100

    fetching data in the frontend through backend api using swr

--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 type Todo struct {
@@ -15,6 +16,11 @@ type Todo struct {
 
 func main() {
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://127.0.0.1:5173",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	todos := []Todo{}
 
