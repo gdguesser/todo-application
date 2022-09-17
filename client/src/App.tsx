4abed2bba4adb6d563af420273da1346c9c4f3a6
Gabriel Guesser <gdguesser@gmail.com>
2022-09-17 14:18:01 +0100

    styling box element and implementing logic to set todo as done; also using icon to show if a todo is done or not

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { Box, List } from "@mantine/core"
+import { Box, Checkbox, List, ThemeIcon } from "@mantine/core"
+import { CheckCircleFillIcon } from "@primer/octicons-react";
 import useSWR from "swr"
 import './App.css'
 import AddTodo from './components/AddTodo'
@@ -19,10 +20,37 @@ function App() {
 
   const {data, mutate} = useSWR<Todo[]>('api/todos', fetcher);
 
-  return <Box>
+  async function markTodoAsDone(id: number) {
+    const updated = await fetch(`${ENDPOINT}/api/todos/${id}/done`, {
+      method: "PATCH"
+    }).then((r) => r.json());
+
+    mutate(updated);
+  }
+
+  return <Box
+    sx={(theme) => ({
+      padding: "2rem",
+      width: "100%",
+      maxWidth: "40rem",
+      margin: "0 auto",
+    })}
+  >
     <List spacing="xs" size="sm" mb={12} center>
       {data?.map((todo) => {
-        return <List.Item key={`todo_list__${todo.id}`}>
+        return <List.Item
+          onClick={() => markTodoAsDone(todo.id)}
+          key={`todo_list__${todo.id}`}
+          icon={
+            todo.done ? (<ThemeIcon color="teal" size={24} radius="xl">
+              <CheckCircleFillIcon size={20}/>
+            </ThemeIcon>
+          ) : (
+            <ThemeIcon color="gray" size={24} radius="xl">
+              <CheckCircleFillIcon size={20}/>
+            </ThemeIcon>)
+          }
+        >
           {todo.title}
         </List.Item>
       })}
